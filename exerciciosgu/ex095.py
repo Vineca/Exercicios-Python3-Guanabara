6ed45aadd0abd23b6ebdadab9a7898f14bcd534e
Vinicius Arantes <vinicius8253@hotmail.com>
2026-09-19 20:03:06 -0300

    Ex 095 resolvido
    Conclui EX095 - cadastro e consulta de jogadores

--- exerciciosgu/ex095.py
+++ exerciciosgu/ex095.py
@@ -5,15 +5,30 @@ while continuar != 'N':
     total = 0
     partidas['nome'] = str(input('Qual o seu nome: '))
     jogos = int(input('Quantos jogos perna de pau? '))
+    partidas['gols'] = []
     for c in range(1,jogos+1):
-        partidas[c] = int(input(f'Quantos gols no jogo {c}: '))
-        total += int(partidas[c])
-    partidas['Total'] = total
+        partidas['gols'].append(int(input(f'Quantos gols no jogo {c}: ')))
+    partidas['Total'] = sum(partidas['gols'])
     lista.append(partidas.copy())
     continuar = str(input('Deseja continuar? [S/N] ')).upper()
 print(lista)
 for n,jog in enumerate(lista):
-    print(f'{n}, {jog["nome"]}')
+    print(f'O jogador {n} : Nome: {jog['nome']}')
+    for l,g in enumerate(jog['gols'], start=1):
+        print(f'No jogo {l} fez {g} gols')
+    print(f'E teve um total de gols: {sum(jog['gols'])}')
+jogador = int(input('Qual jogador [999=quit]: '))
+while jogador != 999:
+    if 0 <= jogador < len(lista):
+        for k,v in lista[jogador].items():
+            if k == 'gols':
+                for pa,go in enumerate(v, start=1):
+                    print(f'No jogo {pa} fez {go} gols')
+        jogador = int(input('Qual jogador [999=quit]: '))
+    else: jogador = int(input('Jogador não existe burro, qual jogador [999=quit]: '))
+
+
+
 
 # print(partidas.values())
 # print(partidas[nome]['Total'])
